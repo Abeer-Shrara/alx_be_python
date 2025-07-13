318b408b7b0cf4fe731394731ada59a4e402e8fc
Abeer <Abeer.Shrara20@gmail.com>
2025-07-13 17:54:53 +0300

    No Comment

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to the varibles
+number1 = 10
+number2 = 5
+# perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+#print the results
+print("Additioin off", number1, "and", number2, "is", addition)
+print("Subtration of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
+
+
