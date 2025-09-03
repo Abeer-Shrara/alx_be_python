78d5901538d7cc06da2824b5e906381ddc93d8f8
Abeer <Abeer.Shrara20@gmail.com>
2025-09-03 16:17:43 +0300

    main1 done

---
+++ oop/main1.py
@@ -0,0 +1,21 @@
+from library_system import Book, EBook, PrintBook, Library
+
+def main():
+    # creat a library instance
+    my_library = Library()
+
+    # creat instanse for each type of books
+    classic_book = Book("Pride and Prejudice", "Jane Austen")
+    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+
+    # Add books to the library
+    my_library.add_book(classic_book)
+    my_library.add_book(digital_novel)
+    my_library.add_book(paper_novel)
+
+    # list all books in the library
+    my_library.list_books()
+
+if __name__ == "__main__":
+    main()  
