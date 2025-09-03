37a18e5dfda885335f6e8d74ec967d47551140b8
Abeer <Abeer.Shrara20@gmail.com>
2025-09-03 14:04:04 +0300

    main added

---
+++ oop/main.py
@@ -0,0 +1,17 @@
+from book_class import book
+
+def main():
+    # creating instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # demonstrating the __str__ method
+    print(my_book)
+
+    # demonstrating the __str__ method
+    print(repr(my_book))
+
+    # deleting a book instance to treger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
